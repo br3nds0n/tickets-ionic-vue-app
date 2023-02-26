7b016574512234697dad203f7b32db9d09b57df2
Brendson Silva <brendson.silva@equipeblue.com.br>
2023-02-26 12:33:57 -0300

    feat: criando estrutura inicial do projeto

---
+++ app/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
---
+++ app/src/main.js
@@ -0,0 +1,59 @@
+import '@ionic/vue/css/core.css';
+
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
+import './theme/variables.css';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+import { IonicVue } from '@ionic/vue';
+
+/* ionic components */
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+  IonIcon,
+  IonRouterOutlet,
+} from '@ionic/vue';
+
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+const app = createApp(App);
+
+app.use(IonicVue);
+app.use(router);
+
+app.component('ExploreContainer', ExploreContainer);
+app.component('IonPage', IonPage);
+app.component('IonHeader', IonHeader);
+app.component('IonToolbar', IonToolbar);
+app.component('IonTitle', IonTitle);
+app.component('IonContent', IonContent);
+app.component('IonTabBar', IonTabBar);
+app.component('IonTabButton', IonTabButton);
+app.component('IonTabs', IonTabs);
+app.component('IonLabel', IonLabel);
+app.component('IonIcon', IonIcon);
+app.component('IonRouterOutlet', IonRouterOutlet);
+
+router.isReady().then(() => {
+  app.mount('#app');
+});
